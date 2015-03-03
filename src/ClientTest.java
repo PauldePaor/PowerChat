5f2225066ba9b62e456f29c9829b71c7e6452b0e
David O'Grady <dogrady92@gmail.com>
2015-03-03 22:55:56 +0000

    Added option to enter server ip address for testing

--- src/ClientTest.java
+++ src/ClientTest.java
@@ -1,10 +1,15 @@
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class ClientTest {
 	public static void main(String[] args){
 		Client charlie;
-		charlie = new Client("127.0.0.1");
+		String ipAddress = JOptionPane.showInputDialog(null, "Enter IP Address of Server [Blank for localhost]");
+		if(ipAddress.equals(""))ipAddress = "127.0.0.1";
+	
+		charlie = new Client(ipAddress);
 		charlie.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		charlie.startRunning();
+
 	}
 }
